134380ab3eba675e78b6cc3556c0317023672604
kamilrzechowski <krzechowski@gmail.com>
2023-01-11 15:31:31 +0100

    docstring with raises exapnd

--- autoxai/explainer/conductance.py
+++ autoxai/explainer/conductance.py
@@ -46,6 +46,7 @@ class LayerConductanceCVExplainer(CVExplainer):
 
         Raises:
             ValueError: if layer is None
+            RuntimeError: if attributions has shape (0)
         """
 
         layer: Optional[torch.nn.Module] = kwargs.get("layer", None)
