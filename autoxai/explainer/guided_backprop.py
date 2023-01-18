30d049a2387a46bfd6f34917bd76bad06fd00a9e
Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>
2023-01-18 15:07:27 +0100

    Add modifying model's ReLU activations
    Signed-off-by: Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>

--- autoxai/explainer/guided_backprop.py
+++ autoxai/explainer/guided_backprop.py
@@ -1,14 +1,29 @@
 """File with Guided Backpropagation algorithm explainer class."""
 
+from abc import abstractmethod
+
 import torch
 from captum.attr import GuidedBackprop
 
 from autoxai.array_utils import validate_result
 from autoxai.explainer.base_explainer import CVExplainer
+from autoxai.explainer.model_utils import modify_modules
 
 
-class GuidedBackpropCVExplainer(CVExplainer):
-    """Guided Backpropagation algorithm explainer."""
+class BaseGuidedBackpropCVExplainer(CVExplainer):
+    """Base Guided Backpropagation algorithm explainer."""
+
+    @abstractmethod
+    def create_explainer(
+        self,
+        model: torch.nn.Module,
+        **kwargs,
+    ) -> GuidedBackprop:
+        """Create explainer object.
+
+        Returns:
+            Explainer object.
+        """
 
     def calculate_features(
         self,
@@ -17,7 +32,7 @@ class GuidedBackpropCVExplainer(CVExplainer):
         pred_label_idx: int,
         **kwargs,
     ) -> torch.Tensor:
-        """Generate features image with Guided Backpropagation algorithm explainer.
+        """Generate features image with Integrated Gradients algorithm explainer.
 
         Args:
             model: Any DNN model You want to use.
@@ -31,7 +46,24 @@ class GuidedBackpropCVExplainer(CVExplainer):
         Raises:
             RuntimeError: if attribution has shape (0).
         """
-        guided_backprop = GuidedBackprop(model=model)
+        guided_backprop = self.create_explainer(model=model)
         attributions = guided_backprop.attribute(input_data, target=pred_label_idx)
         validate_result(attributions=attributions)
         return attributions
+
+
+class GuidedBackpropCVExplainer(BaseGuidedBackpropCVExplainer):
+    """Guided Backpropagation algorithm explainer."""
+
+    def create_explainer(
+        self,
+        model: torch.nn.Module,
+        **kwargs,
+    ) -> GuidedBackprop:
+        """Create explainer object.
+
+        Returns:
+            Explainer object.
+        """
+
+        return GuidedBackprop(model=modify_modules(model))
