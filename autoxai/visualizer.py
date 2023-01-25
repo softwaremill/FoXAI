6278f2dcd3d4e1474e9f0644bcc77aacb5a7f920
Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>
2023-01-25 20:06:22 +0100

    Extract common pre-processing code to a private function
    Signed-off-by: Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>

--- autoxai/visualizer.py
+++ autoxai/visualizer.py
@@ -47,6 +47,36 @@ def generate_figure(
     return figure
 
 
+def _preprocess_img_and_attributes(
+    attributes_matrix: np.ndarray,
+    transformed_img_np: np.ndarray,
+    only_positive_attr: bool,
+) -> Tuple[np.ndarray, np.ndarray]:
+    single_channel_attributes: np.ndarray = normalize_attributes(
+        attributes=attributes_matrix,
+    )
+
+    if only_positive_attr:
+        single_channel_attributes = retain_only_positive(
+            array=single_channel_attributes
+        )
+
+    resized_attributes: np.ndarray = resize_attributes(
+        attributes=single_channel_attributes,
+        dest_height=transformed_img_np.shape[1],
+        dest_width=transformed_img_np.shape[2],
+    )
+
+    # standardize attributes to uint8 type and back-scale them to range 0-1
+    grayscale_attributes = convert_float_to_uint8(resized_attributes) / 255
+
+    # transpoze image from (C x H x W) shape to (H x W x C) to matplotlib imshow
+    normalized_transformed_img = transpose_array(
+        convert_float_to_uint8(transformed_img_np)
+    )
+    return grayscale_attributes, normalized_transformed_img
+
+
 def mean_channels_visualization(
     attributions: torch.Tensor,
     transformed_img: torch.Tensor,
@@ -72,27 +102,10 @@ def mean_channels_visualization(
     attributes_matrix: np.ndarray = attributions.detach().cpu().numpy()
     transformed_img_np: np.ndarray = transformed_img.detach().cpu().numpy()
 
-    single_channel_attributes: np.ndarray = normalize_attributes(
-        attributes=attributes_matrix,
-    )
-
-    if only_positive_attr:
-        single_channel_attributes = retain_only_positive(
-            array=single_channel_attributes
-        )
-
-    resized_attributes: np.ndarray = resize_attributes(
-        attributes=single_channel_attributes,
-        dest_height=transformed_img_np.shape[1],
-        dest_width=transformed_img_np.shape[2],
-    )
-
-    # standardize attributes to uint8 type and back-scale them to range 0-1
-    grayscale_attributes = convert_float_to_uint8(resized_attributes) / 255
-
-    # transpoze image from (C x H x W) shape to (H x W x C) to matplotlib imshow
-    normalized_transformed_img = transpose_array(
-        convert_float_to_uint8(transformed_img_np)
+    grayscale_attributes, normalized_transformed_img = _preprocess_img_and_attributes(
+        attributes_matrix=attributes_matrix,
+        transformed_img_np=transformed_img_np,
+        only_positive_attr=only_positive_attr,
     )
 
     return generate_figure(
@@ -133,6 +146,12 @@ def single_channel_visualization(
     attributes_matrix: np.ndarray = attributions.detach().cpu().numpy()
     transformed_img_np: np.ndarray = transformed_img.detach().cpu().numpy()
     attributes_matrix = attributes_matrix[selected_channel]
+
+    grayscale_attributes, normalized_transformed_img = _preprocess_img_and_attributes(
+        attributes_matrix=attributes_matrix,
+        transformed_img_np=transformed_img_np,
+        only_positive_attr=only_positive_attr,
+    )
 
     single_channel_attributes: np.ndarray = normalize_attributes(
         attributes=attributes_matrix,
