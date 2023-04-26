671a6a6ed5825bf3a7cde4d8ca48baf5f592aaf1
Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>
2023-04-26 14:57:16 +0200

    Fix unit tests for image preprocessing - use rectangle instead of square shapes
    Signed-off-by: Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>

--- tests/explainers/computer_vision/object_detection/test_object_detector.py
+++ tests/explainers/computer_vision/object_detection/test_object_detector.py
@@ -7,9 +7,9 @@ from foxai.explainer.computer_vision.object_detection.base_object_detector impor
 
 
 def test_preprocessing_should_resize_and_transpose_image():
-    img = np.zeros((1, 100, 100, 3))
+    img = np.zeros((1, 200, 100, 3))
     new_shape = (300, 300, 3)
-    expected_shape = (1, 3, 300, 300)
+    expected_shape = (1, 3, 300, 150)
 
     preprocessed_img = BaseObjectDetector.preprocessing(
         img=img,
@@ -23,10 +23,27 @@ def test_preprocessing_should_resize_and_transpose_image():
     assert preprocessed_img.shape == expected_shape
 
 
+def test_preprocessing_should_resize_and_transpose_image_and_change_original_ratio_of_image():
+    img = np.zeros((1, 200, 100, 3))
+    new_shape = (300, 300, 3)
+    expected_shape = (1, 3, 300, 300)
+
+    preprocessed_img = BaseObjectDetector.preprocessing(
+        img=img,
+        new_shape=new_shape,
+        change_original_ratio=True,
+        scaleup=True,
+    )
+
+    assert isinstance(preprocessed_img, torch.Tensor)
+    assert preprocessed_img.dtype == torch.float64
+    assert preprocessed_img.shape == expected_shape
+
+
 def test_preprocessing_should_add_batch_dimension():
-    img = np.zeros((100, 100, 3))
-    new_shape = img.shape
-    expected_shape = (1, 3, 100, 100)
+    img = np.zeros((200, 100, 3))
+    new_shape = (300, 200)
+    expected_shape = (1, 3, 200, 100)
 
     preprocessed_img = BaseObjectDetector.preprocessing(
         img=img,
