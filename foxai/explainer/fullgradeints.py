d69d2f27456d7a6a2b5989245f616ca10bf2cca8
Kamil <49376703+kamilrzechowski@users.noreply.github.com>
2023-05-10 17:17:42 +0200

    Update foxai/explainer/fullgradeints.py
    Co-authored-by: adamwawrzynski <adamwawrzynski@wp.pl>

--- foxai/explainer/fullgradeints.py
+++ foxai/explainer/fullgradeints.py
@@ -43,7 +43,7 @@ _INPLACE_ERROR_MSG: Final[
 ] = """
 Error possibly caused by inplace operations.
 FullGradient does not support inplace operations.
-ReLU in place operations are turned of in the model automatically.
+ReLU inplace operations are turned off in the model automatically.
 Howerer inplace addition, multiplication etc. is the model design
 choice and shlould be avoided in order to make the FullyGradient explainer work
 correctly.
