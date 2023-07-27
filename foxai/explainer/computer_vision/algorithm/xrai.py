616c294801a1a9b189df42fa289cf9408681ddde
Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>
2023-07-27 09:09:49 +0200

    Add parametrization of XRAI algorithm through kwargs
    Signed-off-by: Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>

--- foxai/explainer/computer_vision/algorithm/xrai.py
+++ foxai/explainer/computer_vision/algorithm/xrai.py
@@ -5,7 +5,7 @@ Based on https://github.com/PAIR-code/saliency/blob/master/saliency/core/xrai.py
 """
 from __future__ import absolute_import, division, print_function
 
-from typing import Any, Callable, List, Optional, Tuple, Union
+from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -21,16 +21,10 @@ from foxai.explainer.computer_vision.algorithm.integrated_gradients import (
 )
 from foxai.types import AttributionsType, ModelType
 
-_FELZENSZWALB_SCALE_VALUES: List[int] = [50, 100, 150, 250, 500, 1200]
-_FELZENSZWALB_SIGMA_VALUES: List[float] = [0.8]
-_FELZENSZWALB_IM_RESIZE: Tuple[int, int] = (224, 224)
-_FELZENSZWALB_IM_VALUE_RANGE: List[float] = [-1.0, 1.0]
-_FELZENSZWALB_MIN_SEGMENT_SIZE: int = 150
-
 
 def _normalize_image(
     image_batch: np.ndarray,
-    value_range: List[float],
+    value_range: Tuple[float, float],
     resize_shape: Optional[Tuple[int, int]] = None,
 ) -> np.ndarray:
     """Normalize an image by resizing it and rescaling its values.
@@ -71,9 +65,12 @@ def _normalize_image(
 
 def _get_segments_felzenszwalb(
     image_batch: np.ndarray,
-    resize_image: bool = True,
-    scale_range: Optional[List[float]] = None,
-    dilation_rad: int = 5,
+    scale_range: Tuple[float, float],
+    dilation_rad: int,
+    image_resize: Optional[Tuple[int, int]],
+    sigma_values: Iterable[float],
+    scale_values: Iterable[int],
+    min_segment_size: int,
 ) -> List[List[np.ndarray]]:
     """Compute image segments based on Felzenszwalb's algorithm.
 
@@ -81,35 +78,31 @@ def _get_segments_felzenszwalb(
     and Huttenlocher, D.P. International Journal of Computer Vision, 2004
 
     Args:
-    image: Input image in shape (B x H x W x C).
-    resize_image: If True, the image is resized to 224,224 for the segmentation
-                    purposes. The resulting segments are rescaled back to match
-                    the original image size. It is done for consistency w.r.t.
-                    segmentation parameter range. Defaults to True.
-    scale_range:  Range of image values to use for segmentation algorithm.
-                    Segmentation algorithm is sensitive to the input image
-                    values, therefore we need to be consistent with the range
-                    for all images. If None is passed, the range is scaled to
-                    [-1.0, 1.0]. Defaults to None.
-    dilation_rad: Sets how much each segment is dilated to include edges,
-                    larger values cause more blobby segments, smaller values
-                    get sharper areas. Defaults to 5.
+        image: Input image in shape (B x H x W x C).
+            resize_image:
+        scale_range:  Range of image values to use for segmentation algorithm.
+            Segmentation algorithm is sensitive to the input image
+            values, therefore we need to be consistent with the range
+            for all images. Defaults to [-1.0, 1.0].
+        dilation_rad: Sets how much each segment is dilated to include edges,
+            larger values cause more blobby segments, smaller values
+            get sharper areas.
+        image_resize: If not None, the image is resized to given shape HxW for
+            the segmentation purposes. The resulting segments are rescaled back
+            to match the original image size. It is done for consistency w.r.t.
+            segmentation parameter range.
+        sigma_values: Iterable of sigma values from Felzenszwalb algorithm. Sigma is
+            width (standard deviation) of Gaussian kernel used in preprocessing.
+        scale_values: Iterable of scale values from Felzenszwalb algorithm. It's free
+            parameter. Higher means larger clusters.
+        min_segment_size: Minimum segment size returned by Felzenszwalb algorithm.
     Returns:
         masks: A list of lists of boolean masks as np.ndarrays if size HxW for im size of
                 HxWxC. First level of list has length of batch size.
     """
-    if scale_range is None:
-        scale_range = _FELZENSZWALB_IM_VALUE_RANGE
     # Normalize image value range and size
     original_shape = image_batch.shape[1:3]
-    # TODO (tolgab) This resize is unnecessary with more intelligent param range
-    # selection
-    if resize_image:
-        image_batch = _normalize_image(
-            image_batch, scale_range, _FELZENSZWALB_IM_RESIZE
-        )
-    else:
-        image_batch = _normalize_image(image_batch, scale_range)
+    image_batch = _normalize_image(image_batch, scale_range, image_resize)
 
     if len(image_batch.shape) == 3:
         # add artificial batch size
@@ -118,15 +111,15 @@ def _get_segments_felzenszwalb(
     batch_masks: List[List[np.ndarray]] = []
     for image in image_batch:
         segment_list: List[np.ndarray] = []
-        for scale in _FELZENSZWALB_SCALE_VALUES:
-            for sigma in _FELZENSZWALB_SIGMA_VALUES:
+        for scale in scale_values:
+            for sigma in sigma_values:
                 segment = segmentation.felzenszwalb(
                     image,
                     scale=scale,
                     sigma=sigma,
-                    min_size=_FELZENSZWALB_MIN_SEGMENT_SIZE,
+                    min_size=min_segment_size,
                 )
-                if resize_image:
+                if image_resize:
                     segment = resize(
                         segment,
                         original_shape,
@@ -174,69 +167,6 @@ def _unpack_segs_to_masks(segment_list: List[np.ndarray]) -> List[np.ndarray]:
         for index in range(segment.min(), segment.max() + 1):
             masks.append(segment == index)
     return masks
-
-
-class XRAIParameters(object):
-    """Dictionary of parameters to specify how to XRAI and return outputs."""
-
-    def __init__(
-        self,
-        steps: int = 100,
-        area_threshold: float = 1.0,
-        return_ig_attributions: bool = False,
-        return_xrai_segments: bool = False,
-        flatten_xrai_segments: bool = True,
-    ):
-        # Number of steps to use for calculating the Integrated Gradients
-        # attribution. The higher the number of steps the higher is the precision
-        # but lower the performance. (see also XRAIOutput.error).
-        self.steps = steps
-        # The fraction of the image area that XRAI should calculate the segments
-        # for. All segments that exceed that threshold will be merged into a single
-        # segment. The parameter is used to accelerate the XRAI computation if the
-        # caller is only interested in the top fraction of segments, e.g. 20%. The
-        # value should be in the [0.0, 1.0] range, where 1.0 means that all segments
-        # should be returned.
-        self.area_threshold = area_threshold
-        # If set to True, the XRAI output returns Integrated Gradients attributions
-        # for every baseline. (see XraiOutput.ig_attribution)
-        self.return_ig_attributions = return_ig_attributions
-        # If set to True the XRAI output returns XRAI segments in the order of their
-        # importance. This parameter works in conjunction with the
-        # flatten_xrai_sements parameter. (see also XraiOutput.segments)
-        self.return_xrai_segments = return_xrai_segments
-        # If set to True, the XRAI segments are returned as an integer array with
-        # the same dimensions as the input (excluding color channels). The elements
-        # of the array are set to values from the [1,N] range, where 1 is the most
-        # important segment and N is the least important segment. If
-        # flatten_xrai_sements is set to False, the segments are returned as a
-        # boolean array, where the first dimension has size N. The [0, ...] mask is
-        # the most important and the [N-1, ...] mask is the least important. This
-        # parameter has an effect only if return_xrai_segments is set to True.
-        self.flatten_xrai_segments = flatten_xrai_segments
-        # EXPERIMENTAL - Contains experimental parameters that may change in future.
-        self.experimental_params = {"min_pixel_diff": 50}
-
-
-class XRAIOutput(object):
-    """Dictionary of outputs from a single run of XRAI.GetMaskWithDetails."""
-
-    def __init__(self, attribution_mask: np.ndarray):
-        # The saliency mask of individual input features. For an [HxWx3] image, the
-        # returned attribution is [H,W,1] float32 array. Where HxW are the
-        # dimensions of the image.
-        self.attribution_mask = attribution_mask
-        # Baselines that were used for IG calculation. The shape is [B,H,W,C], where
-        # B is the number of baselines, HxWxC are the image dimensions.
-        self.baselines: Optional[torch.Tensor] = None
-        # IG attributions for individual baselines. The value is set only when
-        # XraiParameters.ig_attributions is set to True. For the dimensions of the
-        # output see XraiParameters.return_ig_for_every _step.
-        self.ig_attribution: Optional[List[AttributionsType]] = None
-        # The result of the XRAI segmentation. The value is set only when
-        # XraiParameters.return_xrai_segments is set to True. For the dimensions of
-        # the output see XraiParameters.flatten_xrai_segments.
-        self.segments: Optional[Union[np.ndarray, List[np.ndarray]]] = None
 
 
 class XRAI:
@@ -292,85 +222,30 @@ class XRAI:
     def get_attribution_mask(
         self,
         input_data: torch.Tensor,
-        call_model_function,
-        pred_label_idx,
-        call_model_args=None,
-        baselines: Optional[torch.Tensor] = None,
-        segments: Optional[List[List[np.ndarray]]] = None,
-        extra_parameters=None,
-    ) -> np.ndarray:
-        """Applies XRAI method on an input image and returns the result saliency heatmap.
-
-        Args:
-            input_data: Input ndarray.
-            call_model_function: A function that interfaces with a model to return
-            specific data in a dictionary when given an input and other arguments.
-            Expected function signature:
-            - call_model_function(input_data_batch,
-                                    call_model_args=None,
-                                    expected_keys=None):
-                input_data_batch - Input for the model, given as a batch (i.e.
-                dimension 0 is the batch dimension, dimensions 1 through n
-                represent a single input).
-                call_model_args - Other arguments used to call and run the model.
-                expected_keys - List of keys that are expected in the output. For
-                this method (XRAI), the expected keys are
-                INPUT_OUTPUT_GRADIENTS - Gradients of the output being
-                    explained (the logit/softmax value) with respect to the input.
-                    Shape should be the same shape as input_data_batch.
-            call_model_args: The arguments that will be passed to the call model
-                function, for every call of the model.
-            baselines: a list of baselines to use for calculating
-                Integrated Gradients attribution. Every baseline in
-                the list should have the same dimensions as the
-                input. If the value is not set then the algorithm
-                will make the best effort to select default
-                baselines. Defaults to None.
-            segments: the lsit of lists of precalculated image segments that should
-                be passed to XRAI. Each element of the list is an
-                [B,N,M] boolean array, where NxM are the image
-                dimensions, B is the batch size. Each elemeent on the list contains
-                exactly the mask that corresponds to one segment. If the value is None,
-                Felzenszwalb's segmentation algorithm will be applied.
-                Defaults to None.
-            extra_parameters: an XRAIParameters object that specifies
-                additional parameters for the XRAI saliency
-                method. If it is None, an XRAIParameters object
-                will be created with default parameters. See
-                XRAIParameters for more details.
-
-        Raises:
-            ValueError: If algorithm type is unknown (not full or fast).
-                        If the shape of `base_attribution` dosn't match the shape of
-                        `input_data`.
-                        If the shape of INPUT_OUTPUT_GRADIENTS doesn't match the
-                        shape of input_data_batch.
-
-        Returns:
-            np.ndarray: A numpy array that contains the saliency heatmap.
-        """
-        results = self.get_mask_with_details(
-            input_data=input_data,
-            call_model_function=call_model_function,
-            call_model_args=call_model_args,
-            pred_label_idx=pred_label_idx,
-            baselines=baselines,
-            segments=segments,
-            extra_parameters=extra_parameters,
-        )
-        return results.attribution_mask
-
-    def get_mask_with_details(
-        self,
-        input_data: torch.Tensor,
         call_model_function: ModelType,
         pred_label_idx: TargetType,
         call_model_args: Any = None,
         baselines: Optional[torch.Tensor] = None,
         segments: Optional[List[List[np.ndarray]]] = None,
-        extra_parameters=None,
-    ) -> XRAIOutput:
-        """Applies XRAI method on an input image and returns detailed information.
+        steps: int = 100,
+        area_threshold: float = 1.0,
+        flatten_xrai_segments: bool = True,
+        min_pixel_diff: int = 50,
+        dilation_rad: int = 5,
+        scale_range: Tuple[float, float] = (-1.0, 1.0),
+        image_resize: Optional[Tuple[int, int]] = (224, 224),
+        sigma_values: Iterable[float] = (0.8,),
+        scale_values: Iterable[int] = (
+            50,
+            100,
+            150,
+            250,
+            500,
+            1200,
+        ),
+        min_segment_size: int = 150,
+    ) -> np.ndarray:
+        """Applies XRAI method on an input image and returns the result saliency heatmap.
 
         Args:
             input_data: Input ndarray.
@@ -404,11 +279,38 @@ class XRAI:
                 exactly the mask that corresponds to one segment. If the value is None,
                 Felzenszwalb's segmentation algorithm will be applied.
                 Defaults to None.
-            extra_parameters: an XRAIParameters object that specifies
-                additional parameters for the XRAI saliency
-                method. If it is None, an XRAIParameters object
-                will be created with default parameters. See
-                XRAIParameters for more details.
+            steps: Number of steps to use for calculating the Integrated Gradients
+                attribution. The higher the number of steps the higher is the precision
+                but lower the performance.
+            area_threshold: The fraction of the image area that XRAI should calculate the
+                segments for. All segments that exceed that threshold will be merged into
+                a single segment. The parameter is used to accelerate the XRAI computation
+                if the caller is only interested in the top fraction of segments, e.g. 20%.
+                The value should be in the [0.0, 1.0] range, where 1.0 means that all
+                segments should be returned.
+            flatten_xrai_segments: If set to True, the XRAI segments are returned as an
+                integer array with the same dimensions as the input (excluding color channels).
+                The elements of the array are set to values from the [1,N] range, where 1
+                is the most important segment and N is the least important segment. If
+                flatten_xrai_sements is set to False, the segments are returned as a
+                boolean array, where the first dimension has size N. The [0, ...] mask is
+                the most important and the [N-1, ...] mask is the least important. This
+                parameter has an effect only if return_xrai_segments is set to True.
+            min_pixel_diff: Do not consider masks that have difference less than
+                this number compared to the current mask. Set it to 1 to remove masks
+                that completely overlap with the current mask. Defaults to 50.
+            image_resize: If not None, the image is resized to given shape HxW for
+                the segmentation purposes. The resulting segments are rescaled back
+                to match the original image size. It is done for consistency w.r.t.
+                segmentation parameter range. Defaults to (224, 224).
+            sigma_values: Iterable of sigma values from Felzenszwalb algorithm. Sigma is
+                width (standard deviation) of Gaussian kernel used in preprocessing.
+                Defaults to (0.8,).
+            scale_values: Iterable of scale values from Felzenszwalb algorithm. It's free
+                parameter. Higher means larger clusters.
+                Defaults to (50, 100, 150, 250, 500, 1200,).
+            min_segment_size: Minimum segment size returned by Felzenszwalb algorithm.
+                Defaults to 150.
 
         Raises:
             ValueError: If algorithm type is unknown (not full or fast).
@@ -418,12 +320,8 @@ class XRAI:
                         shape of input_data_batch.
 
         Returns:
-            XRAIOutput: an object that contains the output of the XRAI algorithm.
+            np.ndarray: A numpy array that contains the saliency heatmap.
         """
-        if extra_parameters is None:
-            extra_parameters = XRAIParameters()
-
-        # Calculate IG attribution if not provided by the caller.
         if baselines is not None:
             x_baselines = self._make_baselines(input_data, baselines)
             baselines = torch.vstack(
@@ -438,7 +336,7 @@ class XRAI:
             call_model_function=call_model_function,
             call_model_args=call_model_args,
             baselines=baselines,
-            steps=extra_parameters.steps,
+            steps=steps,
         )
         # Merge attributions from different baselines.
         attr = np.mean([a.detach().cpu().numpy() for a in attrs], axis=0)
@@ -464,24 +362,26 @@ class XRAI:
         if segments is not None:
             segs = segments
         else:
-            segs = _get_segments_felzenszwalb(x_value_np, dilation_rad=5)
+            segs = _get_segments_felzenszwalb(
+                image_batch=x_value_np,
+                dilation_rad=dilation_rad,
+                scale_range=scale_range,
+                image_resize=image_resize,
+                sigma_values=sigma_values,
+                scale_values=scale_values,
+                min_segment_size=min_segment_size,
+            )
 
-        attr_map, attr_data = self._xrai(
+        attr_map, _ = self._xrai(
             attributes=attr,
             segment_list=segs,
-            area_perc_th=extra_parameters.area_threshold,
-            min_pixel_diff=extra_parameters.experimental_params["min_pixel_diff"],
+            area_perc_th=area_threshold,
+            min_pixel_diff=min_pixel_diff,
             gain_fun=_gain_density,
-            integer_segments=extra_parameters.flatten_xrai_segments,
+            integer_segments=flatten_xrai_segments,
         )
 
-        results = XRAIOutput(attr_map)
-        results.baselines = baselines
-        if extra_parameters.return_xrai_segments:
-            results.segments = attr_data
-        if extra_parameters.return_ig_attributions:
-            results.ig_attribution = attrs
-        return results
+        return attr_map
 
     @staticmethod
     def _xrai(
