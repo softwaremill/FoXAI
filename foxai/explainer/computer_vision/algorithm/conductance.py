Fix baselines computation (#97)
* Add workaround to use multiple explainers of same type in context manager

* Add calculation of mean of aggregated baselines' attributes

* Extract preprocessing baselines parameter to separate function

* Refine docstrings

* Fix preprocess_baselines function, fix typing

* Refine typing

Signed-off-by: Adam Wawrzyński <adam.wawrzynski@reasonfieldlab.com>

foxai/explainer/computer_vision/algorithm/conductance.py
@@ -3,7 +3,7 @@
 Based on https://github.com/pytorch/captum/blob/master/captum/attr/_core/layer/layer_conductance.py.
 """
 
-from typing import Any, Optional
+from typing import Any, List, Optional
 
 import torch
 from captum._utils.typing import BaselineType, TargetType
@@ -11,7 +11,10 @@ from captum.attr import LayerConductance
 
 from foxai.array_utils import validate_result
 from foxai.explainer.base_explainer import Explainer
-from foxai.explainer.computer_vision.model_utils import get_last_conv_model_layer
+from foxai.explainer.computer_vision.model_utils import (
+    get_last_conv_model_layer,
+    preprocess_baselines,
+)
 from foxai.types import AttributionsType, LayerType, ModelType
 
 
@@ -92,13 +95,16 @@ class LayerConductanceCVExplainer(Explainer):
                 Baselines define the starting point from which integral
                 is computed and can be provided as:
 
-                - a single tensor, if inputs is a single tensor, with
-                    exactly the same dimensions as inputs or the first
+                - a single tensor, if input_data is a single tensor, with
+                    exactly the same dimensions as input_data or the first
                     dimension is one and the remaining dimensions match
-                    with inputs.
-
-                - a single scalar, if inputs is a single tensor, which will
-                    be broadcasted for each input value in input tensor.
+                    with input_data.
+                - a batch tensor, if input_data is a batch tensor, with
+                    each tensor of a batch with exactly the same dimensions as
+                    input_data and the first dimension is number of different baselines
+                    to compute and their averaged score. Typical usage of batch
+                    baselines is to provide random baselines and compute mean
+                    attributes from them.
 
                 In the cases when `baselines` is not provided, we internally
                 use zero scalar corresponding to each input tensor.
@@ -168,21 +174,40 @@ class LayerConductanceCVExplainer(Explainer):
             ValueError: if model does not contain conv layers.
             RuntimeError: if attribution has shape (0)
         """
-
+        attributions: AttributionsType
         if layer is None:
             layer = get_last_conv_model_layer(model=model)
 
         conductance = self.create_explainer(model=model, layer=layer)
-        attributions = conductance.attribute(
-            input_data,
+
+        attributions_list: List[AttributionsType] = []
+        baselines_list, aggregate_attributes = preprocess_baselines(
             baselines=baselines,
-            target=pred_label_idx,
-            additional_forward_args=additional_forward_args,
-            n_steps=n_steps,
-            method=method,
-            internal_batch_size=internal_batch_size,
-            return_convergence_delta=False,
-            attribute_to_layer_input=attribute_to_layer_input,
+            input_data_shape=input_data.shape,
         )
-        validate_result(attributions=attributions)
+
+        for baseline in baselines_list:
+            attributions = conductance.attribute(
+                input_data,
+                baselines=baseline,
+                target=pred_label_idx,
+                additional_forward_args=additional_forward_args,
+                n_steps=n_steps,
+                method=method,
+                internal_batch_size=internal_batch_size,
+                return_convergence_delta=False,
+                attribute_to_layer_input=attribute_to_layer_input,
+            )
+            validate_result(attributions=attributions)
+            # if aggregation of attributes is required make sure that dimension of
+            # stacked attributes have baseline number dimension
+            if aggregate_attributes:
+                attributions = attributions.unsqueeze(0)
+
+            attributions_list.append(attributions)
+
+        attributions = torch.vstack(attributions_list)
+        if aggregate_attributes:
+            attributions = torch.mean(attributions, dim=0)
+
         return attributions